a19aac9cc6f38682750850e745af212ef03f5ef4
Anderson freires <andersonfreires1979@gmail.com>
2026-09-25 16:44:26 -0300

    Inicio pagina

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,10 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { Home } from './features/home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [Home],
+  templateUrl: './app.html'
 })
 export class App {
-  protected readonly title = signal('beauty-barbershop-booking');
 }
